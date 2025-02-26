45dca991b262b70f265a6d4f4b0d6aa43fadac13
Muhammad Osama <L1F21BSSE0386@ucp.edu.pk>
2025-02-26 15:52:01 +0500

    implemented Drag and Drop fuctionality

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,12 +7,9 @@ function Home() {
     const [arr, setArr] = useState([]);
     const [editData, setEditData] = useState(null);
 
-    
-
-
     const addData = (obj) => {
         if (editData) {
-            const updatedArr = arr.map((i) => i === editData ? obj : i);
+            const updatedArr = arr.map((i) => (i === editData ? obj : i));
             setArr(updatedArr);
             setEditData(null);
         } else {
@@ -20,7 +17,7 @@ function Home() {
         }
     };
 
-    useEffect(() => { 
+    useEffect(() => {
         const savedData = JSON.parse(localStorage.getItem("data"));
         if (savedData) {
             setArr(savedData);
@@ -28,17 +25,36 @@ function Home() {
     }, []);
 
     useEffect(() => {
-        if(arr.length>0){
-        localStorage.setItem("data", JSON.stringify(arr));
-        console.log("savedData: ",arr); 
-        }       
+        if (arr.length > 0) {
+            localStorage.setItem("data", JSON.stringify(arr));
+        }
     }, [arr]);
 
-    const onDelete = (e) => {
-        const del = arr.filter((index) => index !== e);
-        setArr(del);
-        localStorage.setItem("data",JSON.stringify(del));
-    }
+    const onDelete = (task) => {
+        const updatedArr = arr.filter((i) => i !== task);
+        setArr(updatedArr);
+        localStorage.setItem("data", JSON.stringify(updatedArr));
+    };
+
+    const onDragStart = (e, item) => {
+        e.dataTransfer.setData("task", JSON.stringify(item));
+    };
+
+    const onDragOver = (e) => {
+        e.preventDefault();
+    };
+
+    const onDrop = (e, newStat) => {
+        e.preventDefault();
+        const draggedItem = JSON.parse(e.dataTransfer.getData("task"));
+
+        const updatedArr = arr.map((i) =>
+            JSON.stringify(i) === JSON.stringify(draggedItem) ? { ...i, stat: newStat } : i
+        );
+
+        setArr(updatedArr);
+        localStorage.setItem("data", JSON.stringify(updatedArr));
+    };
 
     return (
         <div className="bg-slate-500">
@@ -46,43 +62,71 @@ function Home() {
             <AddBtn addData={addData} editData={editData} setEditData={setEditData} />
 
             <div className="flex justify-between mx-11">
-
                 {/* TO DO Section */}
-                <div className="bg-blue-200 border border-solid rounded-md w-1/3 h-screen text-center">
+                <div
+                    className="bg-blue-200 border border-solid rounded-md w-1/3 h-screen text-center"
+                    onDragOver={onDragOver}
+                    onDrop={(e) => onDrop(e, "Todo")}
+                >
                     <h1 className="font-semibold text-2xl">TO DO</h1>
-                    <div className=" m-3">
+                    <div className="m-3">
                         {arr
-                            .filter((obj) => obj.stat === "Todo")
+                            .filter((obj) => obj && obj.stat === "Todo")
                             .map((i, index) => (
-                                <Cards key={index} addData={i} onDel={() => { onDelete(i) }} setEditData={() => { setEditData(i) }} />
+                                <Cards
+                                    key={index}
+                                    addData={i}
+                                    onDel={() => onDelete(i)}
+                                    setEditData={() => setEditData(i)}
+                                    onDragStart={(e) => onDragStart(e, i)}
+                                />
                             ))}
                     </div>
                 </div>
 
                 {/* In Progress Section */}
-                <div className="bg-orange-300 border border-solid rounded-md w-1/3 h-screen text-center">
+                <div
+                    className="bg-orange-300 border border-solid rounded-md w-1/3 h-screen text-center"
+                    onDragOver={onDragOver}
+                    onDrop={(e) => onDrop(e, "In-progress")}
+                >
                     <h1 className="font-semibold text-2xl">In Progress</h1>
                     <div className="m-3">
                         {arr
-                            .filter((obj) => obj.stat === "In-progress")
+                            .filter((obj) => obj && obj.stat === "In-progress")
                             .map((i, index) => (
-                                <Cards key={index} addData={i} onDel={() => { onDelete(i) }} setEditData={() => { setEditData(i) }} />
+                                <Cards
+                                    key={index}
+                                    addData={i}
+                                    onDel={() => onDelete(i)}
+                                    setEditData={() => setEditData(i)}
+                                    onDragStart={(e) => onDragStart(e, i)}
+                                />
                             ))}
                     </div>
                 </div>
 
                 {/* Done Section */}
-                <div className="bg-green-500 border border-solid rounded-md w-1/3 h-screen text-center">
+                <div
+                    className="bg-green-500 border border-solid rounded-md w-1/3 h-screen text-center"
+                    onDragOver={onDragOver}
+                    onDrop={(e) => onDrop(e, "Done")}
+                >
                     <h1 className="font-semibold text-2xl">Done</h1>
                     <div className="m-3">
                         {arr
-                            .filter((obj) => obj.stat === "Done")
+                            .filter((obj) => obj && obj.stat === "Done")
                             .map((i, index) => (
-                                <Cards key={index} addData={i} onDel={() => { onDelete(i) }} setEditData={() => { setEditData(i) }} />
+                                <Cards
+                                    key={index}
+                                    addData={i}
+                                    onDel={() => onDelete(i)}
+                                    setEditData={() => setEditData(i)}
+                                    onDragStart={(e) => onDragStart(e, i)}
+                                />
                             ))}
                     </div>
                 </div>
-
             </div>
         </div>
     );
