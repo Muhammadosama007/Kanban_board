618032bff109860916b4c5e2dec4b2d4f31eb846
Muhammad Osama <L1F21BSSE0386@ucp.edu.pk>
2025-03-04 10:39:06 +0500

    chore: app.js updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,27 +1,29 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import React from "react";
+import React, { useContext } from "react";
 import { TokenContext, TokenProvider } from "./context/tokencontext";
 import Home from "./Components/Home";
 import Signup from "./Components/Signup";
 import Login from "./Components/Login";
 
 function App() {
-    return (
-        <TokenProvider>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/signup" element={<Signup />} />
-                </Routes>
-            </Router>
-        </TokenProvider>
-    );
+  //const {token} =useContext(TokenContext);
+  return (
+    <TokenProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
+    </TokenProvider>
+  );
 }
 
 const ProtectedRoute = ({ children }) => {
-    const { token } = React.useContext(TokenContext);
-    return token ? children : <Navigate to="/login" />;
+  const { token } = useContext(TokenContext);
+  
+  return token ? children : <Navigate to="/login" />;
 };
 
 export default App;
