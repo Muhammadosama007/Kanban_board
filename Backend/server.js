c6c980f7c7187be2fc5619aabdce022c3c6f1cbc
Muhammad Osama <L1F21BSSE0386@ucp.edu.pk>
2025-03-05 09:18:56 +0500

    chore: added logRoute logController and updated server.js

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,7 @@ const cookieParse = require('cookie-parser');
 const userAuth = require('../Backend/routes/authentication')
 const userCrud = require('../Backend/routes/user');
 const taskCrud = require('../Backend/routes/task');
+const activityLog= require('../Backend/routes/log');
 
 const app = express();
 
@@ -17,6 +18,7 @@ mongoose.connect('mongodb://localhost:27017/kanban');
 app.use('/auth', userAuth);
 app.use('/user', userCrud);
 app.use('/task', taskCrud);
+app.use('/log',activityLog);
 
 const port = 3002;
 app.listen(port, () => {
